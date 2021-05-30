dd3727ece376fea23fc44e7c44f80df05de0014c
Angelo Zambrano <angelozam17@gmail.com>
2021-05-30 11:05:11 -0500

    feat: remove a note by id

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,13 +1,25 @@
 interface Props {
   description: string;
+  title: string;
+  removeNote: (id: number) => void;
+  id: number;
 }
 
-function Note({ description }: Props) {
+function Note({ description, title, removeNote, id }: Props) {
   return (
-    <li className="card lg:card-side border mb-2">
+    <li className="card lg:card-side border mb-2 hover:shadow">
       <div className="card-body p-4">
-        <h2 className="card-title">No Images</h2>
+        <h2 className="card-title">{title}</h2>
         <p>{description}</p>
+        <div className="card-actions">
+          <button
+            onClick={() => removeNote(id)}
+            className="btn btn-error btn-sm"
+          >
+            Remove note
+          </button>
+          {/* <button className="btn btn-ghost">More info</button> */}
+        </div>
       </div>
     </li>
   );
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { Note } from "./";
-const notes = [
+const initialNotes = [
   {
     id: 1,
     description: "nota uno",
@@ -11,10 +12,26 @@ const notes = [
 ];
 
 function NoteList() {
+  const [notes, setNotes] = React.useState(initialNotes);
+
+  const removeNote = React.useCallback(
+    (id: number) => {
+      const newNotes = notes.filter((n) => n.id !== id);
+      setNotes(newNotes);
+    },
+    [notes]
+  );
+
   return (
     <ul>
       {notes.map(({ id, description }) => (
-        <Note key={id} description={description} />
+        <Note
+          key={id}
+          id={id}
+          description={description}
+          title="Mi title"
+          removeNote={removeNote}
+        />
       ))}
     </ul>
   );
