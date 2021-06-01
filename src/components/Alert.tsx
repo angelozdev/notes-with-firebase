c98a0f71b4dddfac935bdf802eb7f05fdf8a8181
Angelo Zambrano <angelozam17@gmail.com>
2021-06-01 11:22:17 -0500

    style: correct alert banner

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -23,25 +23,27 @@ function Alert({ children, Icon, color }: PropsWithChildren<Props>) {
   };
 
   return (
-    <div className={`alert ${color ? `alert-${color}` : ""}`}>
-      <button
-        onClick={onClick}
-        className={`btn btn-circle btn-xs ${color ? `btn-${color}` : ""}`}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="inline-block w-3 h-3 stroke-current md:w-4 md:h-4"
+    <div className={`alert flex-col ${color ? `alert-${color}` : ""}`}>
+      <div className="md:mb-2">
+        <button
+          onClick={onClick}
+          className={`btn btn-circle btn-xs ${color ? `btn-${color}` : ""}`}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-3 h-3 stroke-current md:w-4 md:h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
 
       <div className="flex-1 self-start">
         <Icon />
